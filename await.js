a95017669e1ea8cff6c72d0cd990a743caf3b6fd
your.user.name <awinoruth@gmail.com>
2023-05-11 14:09:57 +0300

    changes

--- await.js
+++ await.js
@@ -1,3 +1,6 @@
+
+// Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
+// The function should log the message to the console after the specified delay time.
 async function logMessageAfterDelay(message,delay){
     await new Promise(resolve => setTimeout(resolve,delay));
     console.log(message);
@@ -6,6 +9,10 @@ async function logMessageAfterDelay(message,delay){
 console.log(logMessageAfterDelay("Favourite,Food!",3000));
 
 
+
+// You have an array of user IDs and a function getUserData(id) 
+// that returns a Promise with user data when given a user ID. Write an asynchronous function 
+// that fetches and logs the data for each user ID one by one, in sequence.
 
 
 
@@ -26,7 +33,10 @@ fetchAndLogUserData();
 
 
 
-
+// You have an asynchronous function performTask() that returns a Promise. 
+// The Promise resolves if the task is successful and rejects if there's an error. 
+// Write a function that calls performTask() and logs a custom success message if the task 
+// is successful, and a custom error message if there's an error.
 
 
 async function taskHandler() {
